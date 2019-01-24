d36c8d5183d51a70422362d75e09711183f18a11
kixi <kixi73@gmail.com>
2019-01-24 19:24:37 +0100

    domain logic

--- src/clj/tictactoe_svc/domain.clj
+++ src/clj/tictactoe_svc/domain.clj
@@ -1,6 +1,14 @@
 (ns tictactoe-svc.domain)
 
 
+(defn transpose [matrix]
+  (apply mapv vector matrix))
+
+
+(defn mirror [matrix]
+  (mapv #(into [] (reverse %1)) matrix))
+
+
 (defn create-row [val]
   (into [] (repeat 3 val)))
 
@@ -17,27 +25,38 @@
       (assoc-in board [x y] player))))
 
 
-(defn transpose [matrix]
-  (apply mapv vector matrix))
-
-
 (defn winner [evaluated-board]
   (some identity (apply concat evaluated-board)))
 
 
+(defn evaluate-row [row]
+  (reduce (fn [x y] (if (= x y) x nil)) row))
+  
+
 (defn evaluate-rows [board]
   (let [rows (for [r board]
-               (reduce (fn [x y] (if (= x y) x nil)) r))]
+               (evaluate-row r))]
     (for [r rows]
       (into [] (repeat 3 r)))))
 
 
+(defn evaluate-diagonal [board]
+  (let [p (evaluate-row (for [p (range 3)] (get-in board [p p])))]
+    [[p nil nil]
+     [nil p nil]
+     [nil nil p]]))
+
+
 (defn evaluate-board [board]
   (let [r0 (evaluate-rows board)
-        rt (transpose (evaluate-rows (transpose board)))]
-    (if (winner r0)
-      r0
-      rt)))
+        rt (transpose (evaluate-rows (transpose board)))
+        rd0 (evaluate-diagonal board)
+        rdt (mirror (evaluate-diagonal (mirror board)))]
+    (cond
+      (winner r0) r0
+      (winner rt) rt
+      (winner rdt) rdt
+      :else rdt))) 
 
 
 (defn board-full [board]
@@ -69,9 +88,11 @@
        (not (or player-won full?)) (update :turn other-player))))
   
 
+(evaluate-diagonal (mirror [[0 2 1][4 1 6][1 8 9]]))
+
 (-> (create-game :o)
-    (make-move {:player :o :position [0 0]})
+    (make-move {:player :o :position [0 2]})
     (make-move {:player :x :position [0 1]})
-    (make-move {:player :o :position [1 0]})
-    (make-move {:player :x :position [0 2]})
+    (make-move {:player :o :position [1 1]})
+    (make-move {:player :x :position [0 0]})
     (make-move {:player :o :position [2 0]}))
